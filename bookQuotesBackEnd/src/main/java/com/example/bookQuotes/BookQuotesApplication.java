b3a68130931076f0bd2fba359486011c529c5c72
Ibrahim Mourid <workmourid@gmail.com>
2025-09-26 16:14:16 +0800

    just a test

--- bookQuotesBackEnd/src/main/java/com/example/bookQuotes/BookQuotesApplication.java
+++ bookQuotesBackEnd/src/main/java/com/example/bookQuotes/BookQuotesApplication.java
@@ -8,6 +8,8 @@ public class BookQuotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookQuotesApplication.class, args);
+		System.out.println("Hello");
+
 	}
 
 }
