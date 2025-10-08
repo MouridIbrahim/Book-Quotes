2f5b39997827d64d3c307a5571f13a6b19a72e04
Ibrahim Mourid <workmourid@gmail.com>
2025-10-08 23:25:08 +0800

    add theme part

---
+++ bookquotes/lib/core/config/theme/AppColor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const priamary1 = Color(0x00cba736);
+  static const primary2 = Color(0x00121826);
+  static const textColorBlack = Color(0x00232526);
+  static const texyGreyColor = Color(0x006B7280);
+  static const textWhiteColor = Color(0x00E6EDF3);
+}
